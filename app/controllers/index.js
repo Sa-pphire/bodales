1e8000bd1dd6d5ff7222cba4d1ebc778b936fb5b
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2022-12-30 21:11:25 +0100

    second commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,31 +12,18 @@ const createRequest = async (req, res) => {
     let message, transporter;
 
     transporter = nodemailer.createTransport({
-      host: 'mail.privateemail.com',
-      port: 465,
+      service: 'gmail',
       secure: true,
       auth: {
         user: process.env.USER,
         pass: process.env.PASSWORD
-      },
-      tls: {
-        rejectUnauthorized: false
       }
     });
-
-    transporter.verify((error, success) =>{
-      if (error){
-        console.log(error);
-      } else {
-        console.log("Server is ready to send mail");
-      }
-    });
-  
     if (req.body.accountType == 'individual'){
       message = {
     
-        from: 'testmail@bodales.africa',
-        to: 'testmail@bodales.africa',
+        from: process.env.USER,
+        to: process.env.USER,
         subject: 'You have a new request',
         text: 'A new request has been placed by ' + req.body.firstName + " " +  req.body.lastName + ' to move ' + req.body.productDescription + ' from ' + req.body.location + ' to ' + req.body.destination + '.'
       };
@@ -49,15 +36,15 @@ const createRequest = async (req, res) => {
         }
       });
       res.writeHead(302, {
-        'Location': 'https//wa.me/message/CQKJPRAFGNENK1/?text=Hi,%20I%20am%20{{req.body.firstName}}%20{{req.body.lastName}},%20I%20want%20to%20move%20{{req.body.productDescription}}%20from{{req.body.location}}%20to{{req.body.destination}}%20.'
+        'Location': 'https//wa.me/message/CQKJPRAFGNENK1/?text=Hi,%20I%20am%20${req.body.firstName}%20${req.body.lastName},%20I%20want%20to%20move%20${req.body.productDescription}%20from${req.body.location}%20to${req.body.destination}.'
       });
       res.end();
     }
     else {
       message = {
     
-        from: 'testmail@bodales.africa',
-        to: 'testmail@bodales.africa',
+        from: process.env.USER,
+        to: process.env.USER,
         subject: 'You have a new request',
         text: 'A new request has been placed by ' + req.body.companyName + ' to move ' + req.body.productDescription + ' from ' + req.body.location + ' to ' + req.body.destination + '.'
       };
@@ -70,7 +57,7 @@ const createRequest = async (req, res) => {
         }
       });
       res.writeHead(302, {
-        'Location': 'https//wa.me/message/CQKJPRAFGNENK1/?text=Hi,%20I%20am%20{{req.body.companyName}},%20I%20want%20to%20move%20{{req.body.productDescription}}%20from{{req.body.location}}%20to{{req.body.destination}}%20.'
+        'Location': 'https//wa.me/message/CQKJPRAFGNENK1/?text=Hi,%20We%20are%20${req.body.companyName},%20I%20want%20to%20move%20${req.body.productDescription}%20from${req.body.location}%20to${req.body.destination}%20.'
       });
       res.end();
     }
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-const PORT = process.env.PORT;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 }); 
