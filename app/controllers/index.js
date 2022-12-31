b0ea6d83cea77616c6ea68c5d0ba88630d9e17c8
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2022-12-31 09:32:53 +0100

    fix

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -12,54 +12,61 @@ const createRequest = async (req, res) => {
     let message, transporter;
 
     transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'mail.privateemail.com',
+      port: 25,
       secure: true,
       auth: {
         user: process.env.USER,
         pass: process.env.PASSWORD
+      },
+      tls: {
+        rejectUnauthorized: false
       }
     });
+
+    transporter.verify((error, success) =>{
+      if (error){
+        console.log(error);
+      } else {
+        console.log("Server is ready to send mail");
+      }
+    });
+  
     if (req.body.accountType == 'individual'){
       message = {
     
-        from: process.env.USER,
-        to: process.env.USER,
+        from: "hello@bodales.com",
+        to: "hello@bodales.com",
         subject: 'You have a new request',
         text: 'A new request has been placed by ' + req.body.firstName + " " +  req.body.lastName + ' to move ' + req.body.productDescription + ' from ' + req.body.location + ' to ' + req.body.destination + '.'
       };
     
-      transporter.sendMail(message, (err, info) => {
+      transporter.sendMail(message, (err) => {
         if (err){
-          console.log("Error! Email not sent" + info.rejected + info.response);
+          console.log("Error! Email not sent");
         } else{
-          console.log("Email sent successfully!" + info.messageId);
+          console.log("Email sent successfully!");
         }
       });
-      res.writeHead(302, {
-        'Location': 'https//wa.me/message/CQKJPRAFGNENK1/?text=Hi,%20I%20am%20${req.body.firstName}%20${req.body.lastName},%20I%20want%20to%20move%20${req.body.productDescription}%20from${req.body.location}%20to${req.body.destination}.'
-      });
-      res.end();
+    
     }
     else {
       message = {
     
-        from: process.env.USER,
-        to: process.env.USER,
+        from: "hello@bodales.com",
+        to: "hello@bodales.com",
         subject: 'You have a new request',
         text: 'A new request has been placed by ' + req.body.companyName + ' to move ' + req.body.productDescription + ' from ' + req.body.location + ' to ' + req.body.destination + '.'
       };
     
-      transporter.sendMail(message, (err, info) => {
+      transporter.sendMail(message, (err) => {
         if (err){
-          console.log("Error! Email not sent" + info.rejected + info.response);
+          console.log("Error! Email not sent");
         } else{
-          console.log("Email sent successfully!" + info.messageId);
+          console.log("Email sent successfully!");
         }
       });
-      res.writeHead(302, {
-        'Location': 'https//wa.me/message/CQKJPRAFGNENK1/?text=Hi,%20We%20are%20${req.body.companyName},%20I%20want%20to%20move%20${req.body.productDescription}%20from${req.body.location}%20to${req.body.destination}%20.'
-      });
-      res.end();
+
     }
     return res.status(201).json({
       request,
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 }); 
