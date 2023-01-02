f93131662a3d553421b8a70a038fbbefebec6043
Chidinma Judith Akunwa <chidinmaakunwa@gmail.com>
2023-01-02 17:46:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const app = express();
 const routes = require("./app/routes");
 
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/', routes);
