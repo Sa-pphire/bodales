3eaa361d82f142933150a3a8e9dac59f0b784401
sapphire <chidinmaakunwa@gmail.com>
2023-10-08 17:21:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
