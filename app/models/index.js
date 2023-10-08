4891cb8c6be1c256ea3f8f4988de7e054a742e89
sapphire <chidinmaakunwa@gmail.com>
2023-10-08 15:50:23 +0100

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -1,13 +1,6 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-   process.env.DB,
-   process.env.USER,
-   process.env.PASSWORD,
-  {
-    dialect:  "postgres"
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const db = {};
 
